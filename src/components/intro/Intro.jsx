ac7bae778b979e2b2d4421aba8265136a0827c5b
Muhammad Bilal <bilalchanna67@gmail.com>
2024-07-28 13:10:57 +0500

    initial commit

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,40 @@
+import "./intro.scss";
+import { useEffect, useRef } from "react";
+import { init } from "ityped";
+
+export const Intro = () => {
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Developer", "Designer", "Content Creator"],
+    });
+  }, []);
+
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="picture" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Shahbaz Shaheen</h1>
+          <h3>
+            Freelance <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+// export default Intro;
